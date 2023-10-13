e9aab33cb35a5d4222adf9bdaec72f5aa5d47969
Jaime Dargallo <jaimedargallo@gmail.com>
2023-10-13 15:07:06 +0200

    feature welcome function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
-const header = document.querySelector("titulo-blog");
+// Creamos un nuevo parrafo
+const welcomeH2 = document.createElement('p');
+
+// Creamos texto para el parrafo
+const welcome = document.createTextNode('Welcome to '); 
+
+// Agrega el texto al parrafo
+welcomeH2.appendChild(welcome);
+
+// Agregar un nuevo atributo de clase
+welcomeH2.setAttribute('class', 'texto');
+
+// Seleccionar contenedor
+const container = document.getElementsByClassName('titulo-Blog')[0];
+
+// Agregar el nuevo parrafo dentro del contenedor
+container.appendChild(welcomeH2);
